37222d9848ebdb83d792b380e4184f25ca33a951
marcandy <marcandy.nj@gmail.com>
2017-01-04 17:27:58 -0700

    node pract

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json);
+app.use(middleware.addHeaders);
 
 var middleware = app.require('./controllers/middleware.js');
-var user = require('../user.js')
+var middleware = app.require('./controllers/mainCtrl.js');
+var user = require('./user.js')
 
 
 app.listen(3000);
